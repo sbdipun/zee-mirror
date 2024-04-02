e6a093d29a735eb5623ca9ae52a469c0f0421ece
sbdipun <87560288+sbdipun@users.noreply.github.com>
2024-04-02 21:02:08 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -58,10 +58,10 @@ async def start(_, message):
     elif config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Bot Started.\n' \
                        'Now I will send all of your stuffs here.\n' \
-                       'Use me at: @Z_Mirror'
+                       'Hope You are Doing Good'
     elif not config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Sorry, you cannot use me here!\n' \
-                       'Join: @Z_Mirror to use me.\n' \
+                       'Hope You are Doing Good.\n' \
                        'Thank You'
     else:
         tag = message.from_user.mention
